059eba53b120da80e32ae78fb15608489505ad8d
Hudson Green <HudsonGTV@users.noreply.github.com>
2017-11-26 12:13:53 -0800

    Update profile.js

--- js/profile.js
+++ js/profile.js
@@ -5,9 +5,11 @@ $(function() {
   
   var profileName = getUrlParameters("", "", true);
   
+  alert(profileName);
+  
   for(var i = 0; i < users.length; ++i) {
     if(users[i] == profileName.toString()) {
-      //alert("success");
+      alert("success");
       $(".profile-name").innerHTML = profileName;
       break;
     }
