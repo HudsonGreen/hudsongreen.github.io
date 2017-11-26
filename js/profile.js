6730f3b7ff5615616610833e3e71b7e76038f7bb
Hudson Green <HudsonGTV@users.noreply.github.com>
2017-11-26 12:25:54 -0800

    Update profile.js

--- js/profile.js
+++ js/profile.js
@@ -3,8 +3,6 @@
   var users = ["HG"];
   var verifiedUsers = ["HG"];
   
-  alert("TEST");
-  
   var profileName = getUrlParameters("", "", true);
   
   alert(profileName);
@@ -16,5 +14,24 @@
       break;
     }
   }
+  
+  function getUrlParameters(parameter, staticURL, decode){
+
+       var currLocation = (staticURL.length)? staticURL : window.location.search,
+           parArr = currLocation.split("?")[1].split("&"),
+           returnBool = true;
+
+       for(var i = 0; i < parArr.length; i++){
+            parr = parArr[i].split("=");
+            if(parr[0] == parameter){
+                return (decode) ? decodeURIComponent(parr[1]) : parr[1];
+                returnBool = true;
+            }else{
+                returnBool = false;            
+            }
+       }
+
+       if(!returnBool) return false;  
+    }
   
 })();
